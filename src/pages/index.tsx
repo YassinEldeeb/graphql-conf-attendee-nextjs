a2b8f02faa31eaa655edf920e21f651402d761cf
YassinEldeeb <yassineldeeb94@gmail.com>
2023-08-30 18:53:31 -0700

    fix stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,243 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+
+  const { fullName, jobTitle, company } = router.query
+  const searchParam = router.asPath.startsWith('/')
+    ? router.asPath.slice(1)
+    : router.asPath
+
+  const SEOTitle = `${fullName} is attending GraphQL Conf 2023 in San Francisco Bay Area,
+          CA`
+  const SEODescription =
+    'GraphQLConf 2023 hosted by the GraphQL Foundation. September 19-21, 2023. San Francisco Bay Area, California'
+  const SEOImageURL = `https://og-image.the-guild.dev/conf${searchParam}`
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className='bg-[#171e26] min-h-screen flex flex-col'>
+      <Head>
+        <meta
+          name='keywords'
+          content={`GraphQL, GraphQLConf, GraphQLConf 2023 —— ${fullName}, ${jobTitle}, ${company}, attending!`}
+        />
+        <meta
+          property='og:url'
+          content={`https://graphql.org/${searchParam}`}
+        />
+        <title>{SEOTitle}</title>
+        <meta name='description' content={SEODescription} />
+        <meta property='og:title' content={SEOTitle} />
+        <meta property='og:description' content={SEODescription} />
+        <meta property='og:image' content={SEOImageURL} />
+        <meta property='og:image:alt' content={SEODescription} />
+        <meta name='twitter:site' content='@graphql' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={SEOTitle} />
+        <meta name='twitter:description' content={SEODescription} />
+        <meta name='twitter:image' content={SEOImageURL} />
+      </Head>
+
+      <div className='px-16 py-5 border-solid border-b-[#343a46] border-b-[1px] flex justify-between items-center'>
+        <a
+          href='https://graphql.org/conf'
+          className='font-medium hover:underline flex items-center gap-2'
+        >
+          <svg
+            className='w-9 h-9'
+            version='1.1'
+            id='GraphQL_Logo'
+            xmlns='http://www.w3.org/2000/svg'
+            xmlnsXlink='http://www.w3.org/1999/xlink'
+            x='0px'
+            y='0px'
+            viewBox='0 0 400 400'
+            enableBackground='new 0 0 400 400'
+            xmlSpace='preserve'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <g>
+              <g>
+                <g>
+                  <rect
+                    x='122'
+                    y='-0.4'
+                    transform='matrix(-0.866 -0.5 0.5 -0.866 163.3196 363.3136)'
+                    fill='#E535AB'
+                    width='16.6'
+                    height='320.3'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='39.8'
+                    y='272.2'
+                    fill='#E535AB'
+                    width='320.3'
+                    height='16.6'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='37.9'
+                    y='312.2'
+                    transform='matrix(-0.866 -0.5 0.5 -0.866 83.0693 663.3409)'
+                    fill='#E535AB'
+                    width='185'
+                    height='16.6'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='177.1'
+                    y='71.1'
+                    transform='matrix(-0.866 -0.5 0.5 -0.866 463.3409 283.0693)'
+                    fill='#E535AB'
+                    width='185'
+                    height='16.6'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='122.1'
+                    y='-13'
+                    transform='matrix(-0.5 -0.866 0.866 -0.5 126.7903 232.1221)'
+                    fill='#E535AB'
+                    width='16.6'
+                    height='185'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='109.6'
+                    y='151.6'
+                    transform='matrix(-0.5 -0.866 0.866 -0.5 266.0828 473.3766)'
+                    fill='#E535AB'
+                    width='320.3'
+                    height='16.6'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='52.5'
+                    y='107.5'
+                    fill='#E535AB'
+                    width='16.6'
+                    height='185'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='330.9'
+                    y='107.5'
+                    fill='#E535AB'
+                    width='16.6'
+                    height='185'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <rect
+                    x='262.4'
+                    y='240.1'
+                    transform='matrix(-0.5 -0.866 0.866 -0.5 126.7953 714.2875)'
+                    fill='#E535AB'
+                    width='14.5'
+                    height='160.9'
+                  />
+                </g>
+              </g>
+              <path
+                fill='#E535AB'
+                d='M369.5,297.9c-9.6,16.7-31,22.4-47.7,12.8c-16.7-9.6-22.4-31-12.8-47.7c9.6-16.7,31-22.4,47.7-12.8   C373.5,259.9,379.2,281.2,369.5,297.9'
+              />
+              <path
+                fill='#E535AB'
+                d='M90.9,137c-9.6,16.7-31,22.4-47.7,12.8c-16.7-9.6-22.4-31-12.8-47.7c9.6-16.7,31-22.4,47.7-12.8   C94.8,99,100.5,120.3,90.9,137'
+              />
+              <path
+                fill='#E535AB'
+                d='M30.5,297.9c-9.6-16.7-3.9-38,12.8-47.7c16.7-9.6,38-3.9,47.7,12.8c9.6,16.7,3.9,38-12.8,47.7   C61.4,320.3,40.1,314.6,30.5,297.9'
+              />
+              <path
+                fill='#E535AB'
+                d='M309.1,137c-9.6-16.7-3.9-38,12.8-47.7c16.7-9.6,38-3.9,47.7,12.8c9.6,16.7,3.9,38-12.8,47.7   C340.1,159.4,318.7,153.7,309.1,137'
+              />
+              <path
+                fill='#E535AB'
+                d='M200,395.8c-19.3,0-34.9-15.6-34.9-34.9c0-19.3,15.6-34.9,34.9-34.9c19.3,0,34.9,15.6,34.9,34.9   C234.9,380.1,219.3,395.8,200,395.8'
+              />
+              <path
+                fill='#E535AB'
+                d='M200,74c-19.3,0-34.9-15.6-34.9-34.9c0-19.3,15.6-34.9,34.9-34.9c19.3,0,34.9,15.6,34.9,34.9   C234.9,58.4,219.3,74,200,74'
+              />
+            </g>
+          </svg>
+          GraphQL Conf 2023
+        </a>
+        <div className='text-[#bcc1cd] font-medium text-sm flex gap-6'>
+          <a
+            className='hover:underline'
+            href='https://graphql.org/conf/speakers/'
+          >
+            Speakers
+          </a>
+          <a
+            className='hover:underline'
+            href='https://graphql.org/conf/schedule/'
+          >
+            Schedule
           </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className='h-full flex px-[10%] items-center flex-1 justify-between lg:flex-row flex-col py-10 lg:py-0'>
+        <div className='lg:mb-9 mb-0'>
+          <h1 className='lg:text-5xl text-3xl font-bold'>
+            {fullName}'s Ticket
+          </h1>
+          <div className='text-xl pt-[24px] pb-[45px]'>
+            Join me <span className='font-medium'>September 19-21, 2023</span>.
+            In <span className='font-medium'>San Francisco Bay Area, CA</span>
+          </div>
+          <a
+            href='https://cvent.me/4zbxz9'
+            className='px-6 py-3.5 bg-[#f6009b] rounded-md w-max font-medium'
+          >
+            Get Your Ticket!
+          </a>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='ticket lg:max-w-[45%] max-w-[100%]'>
+          <div className='left'></div>
+          <div className='right'></div>
+          <div className='ticket-content-wrapper'>
+            <img src={SEOImageURL} className='rounded-[15px]'></img>
+          </div>
+        </div>
       </div>
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async () => {
+  return {
+    props: {},
+  }
 }
